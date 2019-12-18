96a2e7bf4f51e68618fe2568b359a75ff5c0e92f
aruppert <aruppert@pm.me>
2019-12-18 17:04:19 +0100

    Prettifying files

--- client/src/stories/0-Welcome.stories.js
+++ client/src/stories/0-Welcome.stories.js
@@ -3,11 +3,11 @@ import { linkTo } from '@storybook/addon-links';
 import { Welcome } from '@storybook/react/demo';
 
 export default {
-  title: 'Welcome',
+  title: 'Welcome'
 };
 
 export const toStorybook = () => <Welcome showApp={linkTo('Button')} />;
 
 toStorybook.story = {
-  name: 'to Storybook',
+  name: 'to Storybook'
 };
