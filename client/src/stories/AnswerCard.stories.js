de5a8e5b30f78b1ba1deef3be650a96e4d3df101
aruppert <56864263+aruppert@users.noreply.github.com>
2019-12-19 11:56:15 +0100

    Add AnswerCard component to storybook (#30)
    * Add AnswerCard and cleaned up storybook demos
    
    * Add more colors and changed background

---
+++ client/src/stories/AnswerCard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AnswerCard from '../components/AnswerCard';
+
+export default {
+  title: 'AnswerCard'
+};
+
+export function Card() {
+  return <AnswerCard>Test</AnswerCard>;
+}
--- client/src/themes/primary.js
+++ client/src/themes/primary.js
@@ -1,6 +1,10 @@
 const primary = {
   colors: {
-    background: '#F2F2F2'
+    text1: '#fff',
+    text2: '#000',
+    background: '#F2F2F2',
+    card1: '#04DBC6',
+    card2: '#007E71'
   }
 };
 
