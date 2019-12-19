7e34bd28ad1acfb9a05d4d3a6e88f615681e0dc9
aruppert <aruppert@pm.me>
2019-12-19 19:08:31 +0100

    Add three card components

---
+++ client/src/stories/AddQuestionsCard.stories.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import AddQuestionsCard from '../components/AddQuestionsCard';
+
+export default {
+  title: 'AddQuestionsCard'
+};
+
+export function Card() {
+  return <AddQuestionsCard></AddQuestionsCard>;
+}
