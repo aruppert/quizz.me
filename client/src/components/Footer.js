689274682be18862a6db386d36c0c350725df5c1
aruppert <56864263+aruppert@users.noreply.github.com>
2020-01-05 15:28:18 +0100

    Update footer with highscore and middle button (#72)

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 import Dice from '../icons/Dice';
 import AddQuestion from '../icons/AddQuestion';
 import ButtonLink from './ButtonLink';
+import Highscore from '../icons/HighScore';
 
 const FooterBar = styled.div`
   position: absolute;
@@ -37,12 +38,44 @@ const FooterButton = styled(ButtonLink)`
   justify-content: center;
 `;
 
+const DiceButton = styled(ButtonLink)`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 75px;
+  height: 75px;
+  background: linear-gradient(
+    to right,
+    ${props => props.theme.colors.card2},
+    ${props => props.theme.colors.card1}
+  );
+  border-radius: 50%;
+  outline: none;
+  border: none;
+`;
+
+const DiceButtonBackground = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  width: 80px;
+  height: 80px;
+  background: ${props => props.theme.colors.background};
+  border-radius: 50%;
+  margin-bottom: 20px;
+`;
+
 export default function Footer() {
   return (
     <FooterBar>
       <FooterButton to="/choose">
-        <Dice />
+        <Highscore />
       </FooterButton>
+      <DiceButtonBackground>
+        <DiceButton to="/choose">
+          <Dice />
+        </DiceButton>
+      </DiceButtonBackground>
       <FooterButton to="/puborprivate">
         <AddQuestion />
       </FooterButton>
