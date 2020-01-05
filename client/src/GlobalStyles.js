bf490a94fb47ea5dc964700afd5833320b0a4e1c
aruppert <56864263+aruppert@users.noreply.github.com>
2020-01-05 17:36:45 +0100

    Add final fonts (#74)

--- client/src/GlobalStyles.js
+++ client/src/GlobalStyles.js
@@ -12,7 +12,18 @@ function GlobalStyles() {
         *:after {
           box-sizing: border-box;
         }
+        @font-face {
+          font-family: 'Gilroy';
+          src: url('./fonts/gilroy-extrabold.eot');
+          src: url('./fonts/gilroy-extrabold.eot?#iefix') format('embedded-opentype'),
+            url('./fonts/gilroy-extrabold.otf') format('opentype'),
+            url('./fonts/gilroy-extrabold.svg') format('svg'),
+            url('./fonts/gilroy-extrabold.ttf') format('truetype'),
+            url('./fonts/gilroy-extrabold.woff') format('woff'),
+            url('./fonts/gilroy-extrabold.woff2') format('woff2');
+        }
         body {
+          font-family: 'Gilroy';
           font-size: 16px;
           margin: 0;
           background: ${theme.colors.background};
